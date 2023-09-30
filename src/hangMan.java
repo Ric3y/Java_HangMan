9e49fd077ef79481bf0cab1de15ff1938a2366df
Ric3y <truonghoangtampro@gmail.com>
2023-09-30 11:48:17 -0500

    Add censor word and start game basics

--- src/hangMan.java
+++ src/hangMan.java
@@ -3,13 +3,37 @@ import java.util.*;
 
 public class hangMan {
     public static void main(String[] args) throws FileNotFoundException {
-        //test stuff
+        //Variable initialization
+        Scanner scanner = new Scanner(System.in);
         ArrayList<String> wordsArray = new ArrayList<>();
         String randomWord;
+        int guessLeft = 7;
+        char guess;
 
+        //Call function to get read in file and get random word 
         wordsArray = readFile("wordList.txt");
         randomWord = randomWordGenerator(wordsArray);
-        System.out.println(randomWord);
+        
+        //censor random word with underscores 
+        //censorWord = "_____"
+        char[] censorWord = randomWord.toCharArray();
+        for(int i = 0; i < censorWord.length; i++) {
+            censorWord[i] = '_';
+        }
+
+        //Start of game
+        System.out.println("Welcome to Hang Man"); 
+        System.out.println("You will have 7 attemps to guess a random 5 letters word");
+        System.out.print("Random word is: " + randomWord); //remove later 
+
+        while (guessLeft > 0) {
+            System.out.print("Enter your guess with a character");
+            guess = scanner.next().charAt(1);
+            checkGuess(guess);
+        }
+
+
+        scanner.close();
     }
 
     /** 
@@ -33,7 +57,7 @@ public class hangMan {
             }
             reader.close();
         } catch (Exception e) {
-            // Handle any exceptions when file does not exist
+            // Handle any exceptions when file does not exist.
             System.err.println("An error occurred while reading the file: " + e.getMessage());
         }
         return wordsArray;
@@ -48,5 +72,12 @@ public class hangMan {
         int randomIndex = random.nextInt(wordsArray.size());
         String randomWord = wordsArray.get(randomIndex);
         return randomWord;
+    }
+
+    /**
+    * Check user input to word, main game logic here 
+    */
+    public static void checkGuess(char guess) {
+        guess--;
     }
 }
